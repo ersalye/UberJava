3be92b90402a6449c19d904926af48fe9dedbf74
Hiep Nguyen Van <hiepmacbook@Hieps-MacBook-Pro.local>
2020-09-16 23:44:19 +0700

    Unit 8 Driver change avatar

---
+++ Unit/Unit_8 Driver change avatar/app/src/main/java/com/example/uberdriver/Common.java
@@ -0,0 +1,23 @@
+package com.example.uberdriver;
+
+import com.example.uberdriver.Model.DriverInfoModel;
+
+public class Common {
+
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+    public static final String DRIVERS_LOCATION_REFERENCES = "DriversLocation";
+
+    public static DriverInfoModel currentUser;
+
+    public static String buildWelcomeMessage() {
+        if (Common.currentUser != null)
+        {
+            return new StringBuilder("Welcome ")
+                    .append(Common.currentUser.getFirstName())
+                    .append(" ")
+                    .append(Common.currentUser.getLastName()).toString();
+        }
+        else
+            return "";
+    }
+}
