4dc8bcb3084eefc22ccafdb71b9c2395e62a38b6
Hiep Nguyen Van <hiepmacbook@Hieps-MacBook-Pro.local>
2020-09-16 16:36:47 +0700

    Unit 5 Driver location

---
+++ Unit/Unit_5 Driver location/app/src/main/java/com/example/uberdriver/Common.java
@@ -0,0 +1,10 @@
+package com.example.uberdriver;
+
+import com.example.uberdriver.Model.DriverInfoModel;
+
+public class Common {
+
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+
+    public static DriverInfoModel currentUser;
+}
