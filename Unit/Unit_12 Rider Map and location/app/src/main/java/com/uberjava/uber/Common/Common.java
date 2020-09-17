11ff563a566df1d0967be063d5d261ecf3654514
Hiep Nguyen Van <hiepmacbook@Hieps-MacBook-Pro.local>
2020-09-17 16:45:52 +0700

    Unit 12 Rider map and location

---
+++ Unit/Unit_12 Rider Map and location/app/src/main/java/com/uberjava/uber/Common/Common.java
@@ -0,0 +1,9 @@
+package com.uberjava.uber.Common;
+
+import com.uberjava.uber.Model.RiderModel;
+
+public class Common {
+
+    public static final String DRIVER_INFO_REFERENCE = "Riders";
+    public static RiderModel currentRider;
+}
